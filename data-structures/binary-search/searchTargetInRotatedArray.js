37be5f284877529491547b530ccd3480c58dd483
ShakthivelRameshNirmala <rn.shakthivel@gmail.com>
2024-09-14 10:28:18 -0500

    Search in Rotated Array

---
+++ data-structures/binary-search/searchTargetInRotatedArray.js
@@ -0,0 +1,25 @@
+function searchTargetInRotatedArray(nums, target) {
+    let left = 0, right = nums.length - 1
+
+    while (left <= right) {
+        const mid = left + Math.floor((right - left) / 2)
+
+        if (target === nums[mid]) {
+            return mid
+        } else if (nums[mid] >= nums[left]) {
+            if (target >= nums[left] && target < nums[mid]) {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        } else {
+            if (target <= nums[right] && target > nums[mid]) {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+    }
+
+    return -1
+}
