67743d76e7156c057c12221b46bbbb59a169e838
ShakthivelRameshNirmala <rn.shakthivel@gmail.com>
2024-09-27 08:57:24 -0500

    Can Jump Bottom Up Approach

--- data-structures/greedy/canJump.js
+++ data-structures/greedy/canJump.js
@@ -1,3 +1,4 @@
+// Top Down Approach
 function canJump(nums) {
   const UNKNOWN = 0;
   const BAD = 1;
@@ -28,4 +29,25 @@ function canJump(nums) {
   }
 
   return canJumpFromPosition(0, nums);
+}
+
+// Bottom Up Approach
+function canJump(nums) {
+  const UNKOWN = 0,
+    GOOD = 2;
+  const memo = Array.from({ length: nums.length }, () => UNKOWN);
+  memo[memo.length - 1] = GOOD;
+
+  for (let i = nums.length - 2; i >= 0; i--) {
+    const furthestJump = Math.min(i + nums[i], nums.length - 1);
+
+    for (let j = i + 1; j <= furthestJump; j++) {
+      if (memo[j] === GOOD) {
+        memo[i] = GOOD;
+        break;
+      }
+    }
+  }
+
+  return memo[0] === GOOD;
 }
