c197b6babca78e03313ab199207c3cca9a189138
ShakthivelRameshNirmala <rn.shakthivel@gmail.com>
2024-10-07 08:30:37 -0500

    Cloning Graph BFS

--- data-structures/graphs/cloningGraph.js
+++ data-structures/graphs/cloningGraph.js
@@ -15,3 +15,26 @@ function cloningGraph(node) {
 
   return dfs(node);
 }
+
+function cloningGraph(node) {
+  if (node === null) return null;
+
+  const visited = new Map();
+  const queue = [node];
+  visited.set(node, new Node(node.val));
+
+  while (queue.length) {
+    const node = queue.shift();
+
+    for (const neighbor of node.neighbors) {
+      if (!visited.has(neighbor)) {
+        visited.set(neighbor, new Node(neighbor.val));
+        queue.push(neighbor);
+      }
+
+      visited.get(node).neighbors.push(visited.get(neighbor));
+    }
+  }
+
+  return visited.get(node);
+}
