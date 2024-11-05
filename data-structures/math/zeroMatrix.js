9e589a196ad6c8674aabdb1ada04d6b2905a1d7d
ShakthivelRameshNirmala <rn.shakthivel@gmail.com>
2024-11-05 06:56:53 -0600

    Zero Matrix

--- data-structures/math/zeroMatrix.js
+++ data-structures/math/zeroMatrix.js
@@ -46,3 +46,43 @@ function zeroMatrix(matrix) {
     }
   }
 }
+
+function zeroMatrix(matrix) {
+  const ROWS = matrix.length,
+    COLS = matrix[0].length;
+  let rowZero = false;
+
+  for (let row = 0; row < ROWS; row++) {
+    for (let col = 0; col < COLS; col++) {
+      if (matrix[row][col] === 0) {
+        matrix[0][col] = 0;
+
+        if (row > 0) {
+          matrix[row][0] = 0;
+        } else {
+          rowZero = true;
+        }
+      }
+    }
+  }
+
+  for (let row = 1; row < ROWS; row++) {
+    for (let col = 1; col < COLS; col++) {
+      if (matrix[0][col] === 0 || matrix[row][0] === 0) {
+        matrix[row][col] = 0;
+      }
+    }
+  }
+
+  if (matrix[0][0] === 0) {
+    for (let row = 0; row < ROWS; row++) {
+      matrix[row][0] = 0;
+    }
+  }
+
+  if (rowZero) {
+    for (let col = 0; col < COLS; col++) {
+      matrix[0][col] = 0;
+    }
+  }
+}
