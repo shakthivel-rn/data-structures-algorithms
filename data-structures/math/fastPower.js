0c656c64efe22ed72636a8932ce6c6de55ed05e6
ShakthivelRameshNirmala <rn.shakthivel@gmail.com>
2024-11-05 07:31:41 -0600

    Fast Power Iterative

--- data-structures/math/fastPower.js
+++ data-structures/math/fastPower.js
@@ -12,3 +12,21 @@ function fastPower(x, n) {
   const result = helper(x, Math.abs(n));
   return n >= 0 ? result : 1 / result;
 }
+
+function fastPower(x, n) {
+  if (x === 0) return 0;
+
+  if (n === 0) return 1;
+  if (n === 1) return x;
+
+  let power = Math.abs(n);
+  let result = 1;
+
+  while (power) {
+    if (power % 2 === 1) result *= x;
+    x *= x;
+    power = Math.floor(power / 2);
+  }
+
+  return n >= 0 ? result : 1 / result;
+}
