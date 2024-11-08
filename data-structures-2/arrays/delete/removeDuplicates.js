489d852e457554a792a01b650abfcaa978b2ac21
ShakthivelRameshNirmala <rn.shakthivel@gmail.com>
2024-11-08 07:09:12 -0600

    Remove Duplicates With Constant Space

--- data-structures-2/arrays/delete/removeDuplicates.js
+++ data-structures-2/arrays/delete/removeDuplicates.js
@@ -13,6 +13,19 @@ function removeDuplicates(nums) {
   return insertIndex;
 }
 
+function removeDuplicates(nums) {
+  let insertIndex = 1;
+
+  for (let index = 1; index < nums.length; index++) {
+    if (nums[index] !== nums[index - 1]) {
+      nums[insertIndex] = nums[index];
+      insertIndex += 1;
+    }
+  }
+
+  return insertIndex;
+}
+
 function swap(nums, index1, index2) {
   [nums[index1], nums[index2]] = [nums[index2], nums[index1]];
 }
