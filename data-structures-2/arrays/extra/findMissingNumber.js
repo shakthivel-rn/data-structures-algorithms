23a4127c0c00e0c7d4616336a6fef6e6bf29bbae
ShakthivelRameshNirmala <rn.shakthivel@gmail.com>
2024-11-10 11:38:25 -0600

    Find Missing Number

--- data-structures-2/arrays/extra/findMissingNumber.js
+++ data-structures-2/arrays/extra/findMissingNumber.js
@@ -1,4 +1,4 @@
-function findDisappearedNumbers(nums) {
+function findMissingNumber(nums) {
   const seen = new Set();
   const result = [];
 
@@ -10,6 +10,26 @@ function findDisappearedNumbers(nums) {
 
   for (let num = 1; num < nums.length + 1; num++) {
     if (!seen.has(num)) {
+      result.push(num);
+    }
+  }
+
+  return result;
+}
+
+function findMissingNumber(nums) {
+  const result = [];
+
+  for (let index = 0; index < nums.length; index++) {
+    const newIndex = Math.abs(nums[index]) - 1;
+
+    if (nums[newIndex] > 0) {
+      nums[newIndex] *= -1;
+    }
+  }
+
+  for (let num = 1; num < nums.length + 1; num++) {
+    if (nums[num - 1] > 0) {
       result.push(num);
     }
   }
