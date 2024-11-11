2b5255ad13c6822308abc3fd0ed3ed095aa131f2
ShakthivelRameshNirmala <rn.shakthivel@gmail.com>
2024-11-11 07:46:22 -0600

    Selection Sort

---
+++ data-structures-2/sorting/selectionSort.js
@@ -0,0 +1,17 @@
+function selectionSort(arr) {
+  const LENGTH = arr.length;
+
+  for (let i = 0; i < LENGTH; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < LENGTH; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) [arr[minIndex], arr[i]] = [arr[i], arr[minIndex]];
+  }
+
+  return arr;
+}
