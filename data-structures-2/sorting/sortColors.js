7f53b276bf38cad50045aacdc743006776a46ffa
ShakthivelRameshNirmala <rn.shakthivel@gmail.com>
2024-11-13 07:37:28 -0600

    Sort Colors using Counting Sort

--- data-structures-2/sorting/sortColors.js
+++ data-structures-2/sorting/sortColors.js
@@ -22,3 +22,29 @@ function sortColors(nums) {
 function swap(nums, index1, index2) {
   [nums[index1], nums[index2]] = [nums[index2], nums[index1]];
 }
+
+function sortColors(nums) {
+  if (nums.length === 0) return nums;
+
+  const max = Math.max(...nums);
+  const min = Math.min(...nums);
+
+  const range = max - min + 1;
+  const count = Array.from({ length: range }, () => 0);
+
+  for (let num of nums) {
+    count[num - min] += 1;
+  }
+
+  let index = 0;
+  for (let k = 0; k < range; k++) {
+    while (count[k] > 0) {
+      nums[index] = k + min;
+
+      index += 1;
+      count[k] -= 1;
+    }
+  }
+
+  return nums;
+}
