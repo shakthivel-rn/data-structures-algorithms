07e8b0f8bbbf439120843e249365e3335a3ae201
ShakthivelRameshNirmala <rn.shakthivel@gmail.com>
2024-11-13 06:53:52 -0600

    Find Kth Largest using Heap Sort

--- data-structures-2/sorting/findKthLargest.js
+++ data-structures-2/sorting/findKthLargest.js
@@ -108,3 +108,35 @@ class MinHeap {
     return this.minHeap.length === 0;
   }
 }
+
+function findKthLargest(nums, k) {
+  const n = nums.length;
+
+  for (let i = Math.floor(n / 2); i >= 0; i--) {
+    heapify(nums, n, i);
+  }
+
+  for (let i = n - 1; i > 0; i--) {
+    [nums[0], nums[i]] = [nums[i], nums[0]];
+
+    if (i === n - k) return nums[i];
+
+    heapify(nums, i, 0);
+  }
+
+  return nums[0];
+}
+
+function heapify(nums, n, i) {
+  let largest = i;
+  const left = 2 * i + 1,
+    right = 2 * i + 2;
+
+  if (left < n && nums[left] > nums[largest]) largest = left;
+  if (right < n && nums[right] > nums[largest]) largest = right;
+
+  if (largest !== i) {
+    [nums[i], nums[largest]] = [nums[largest], nums[i]];
+    heapify(nums, n, largest);
+  }
+}
