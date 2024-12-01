3cc34ba36636a5da54bd8c9f1a9ef7972c2f9333
ShakthivelRameshNirmala <rn.shakthivel@gmail.com>
2024-12-01 16:15:38 -0600

    Find Next Greater Character

--- data-structures-2/searching/findNextGreaterCharacter.js
+++ data-structures-2/searching/findNextGreaterCharacter.js
@@ -15,3 +15,21 @@ function findNextGreaterCharacter(letters, target) {
 
   return letters[right] ? letters[right] : letters[0];
 }
+
+function findNextGreaterCharacter(letters, target) {
+  let left = 0,
+    right = letters.length - 1;
+
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+    const value = letters[mid];
+
+    if (target >= value) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return letters[left % letters.length];
+}
