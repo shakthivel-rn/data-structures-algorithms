121f182d347a94c973744925b12241dcd0c7b3bd
ShakthivelRameshNirmala <rn.shakthivel@gmail.com>
2024-12-01 17:22:09 -0600

    Intersection Array

--- data-structures-2/searching/intersectionArray.js
+++ data-structures-2/searching/intersectionArray.js
@@ -21,3 +21,27 @@ function intersectionArray(nums1, nums2) {
 
   return [...result];
 }
+
+function intersectionArray(nums1, nums2) {
+  nums1.sort((a, b) => a - b);
+  nums2.sort((a, b) => a - b);
+
+  let i = 0,
+    j = 0;
+  const result = [];
+
+  while (i < nums1.length && j < nums2.length) {
+    if (nums1[i] === nums2[j]) {
+      result.push(nums1[i]);
+
+      i += 1;
+      j += 1;
+    } else if (nums1[i] < nums2[j]) {
+      i += 1;
+    } else {
+      j += 1;
+    }
+  }
+
+  return result;
+}
