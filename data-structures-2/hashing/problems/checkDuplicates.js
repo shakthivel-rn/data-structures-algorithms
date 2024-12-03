0e5b688425c1a1cf04dc7a494066e487201c4758
ShakthivelRameshNirmala <rn.shakthivel@gmail.com>
2024-12-03 12:04:11 -0600

    Check Duplicates

---
+++ data-structures-2/hashing/problems/checkDuplicates.js
@@ -0,0 +1,12 @@
+function checkDuplicates(nums) {
+  const seen = new Set();
+
+  for (const num of nums) {
+    if (seen.has(num)) {
+      return true;
+    }
+    seen.add(num);
+  }
+
+  return false;
+}
