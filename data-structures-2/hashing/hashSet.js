c2edc3e82ff8abd4c43acacf0bf4c9870e48d2a4
ShakthivelRameshNirmala <rn.shakthivel@gmail.com>
2024-12-03 09:40:45 -0600

    Hash Set LinkedList

--- data-structures-2/hashing/hashSet.js
+++ data-structures-2/hashing/hashSet.js
@@ -1,3 +1,4 @@
+// Array
 var MyHashSet = function () {
   this.size = 1000;
   this.buckets = Array.from({ length: this.size }, () => []);
@@ -32,3 +33,76 @@ MyHashSet.prototype.contains = function (key) {
 MyHashSet.prototype.hash = function (key) {
   return key % this.size;
 };
+
+// Linked List
+var MyHashSet = function () {
+  this.size = 1000;
+  this.buckets = Array.from({ length: this.size }, () => new Bucket());
+};
+
+MyHashSet.prototype.add = function (key) {
+  const index = this.hash(key);
+  this.buckets[index].insert(key);
+};
+
+MyHashSet.prototype.remove = function (key) {
+  const index = this.hash(key);
+  this.buckets[index].delete(key);
+};
+
+MyHashSet.prototype.contains = function (key) {
+  const index = this.hash(key);
+  return this.buckets[index].exists(key);
+};
+
+MyHashSet.prototype.hash = function (key) {
+  return key % this.size;
+};
+
+class Node {
+  constructor(value, nextNode) {
+    this.value = value;
+    this.next = nextNode;
+  }
+}
+
+class Bucket {
+  constructor() {
+    this.head = new Node();
+  }
+
+  insert(value) {
+    if (!this.exists(value)) {
+      const newNode = new Node(value, this.head.next);
+      this.head.next = newNode;
+    }
+  }
+
+  delete(value) {
+    let prev = this.head,
+      curr = this.head.next;
+
+    while (curr) {
+      if (curr.value === value) {
+        prev.next = curr.next;
+        return;
+      }
+
+      prev = prev.next;
+      curr = curr.next;
+    }
+  }
+
+  exists(value) {
+    let curr = this.head.next;
+
+    while (curr) {
+      if (curr.value === value) {
+        return true;
+      }
+      curr = curr.next;
+    }
+
+    return false;
+  }
+}
