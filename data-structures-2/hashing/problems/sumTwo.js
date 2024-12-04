ac6657820a9d6427e26f3f8e51deb3cd3cdd987d
ShakthivelRameshNirmala <rn.shakthivel@gmail.com>
2024-12-04 14:34:13 -0600

    Sum Two Map

--- data-structures-2/hashing/problems/sumTwo.js
+++ data-structures-2/hashing/problems/sumTwo.js
@@ -11,3 +11,19 @@ function sumTwo(nums, target) {
 
   return [-1, -1];
 }
+
+function sumTwo(nums, target) {
+  const map = new Map();
+
+  for (let index = 0; index < nums.length; index++) {
+    const complement = target - nums[index];
+
+    if (map.has(complement)) {
+      return [index, map.get(complement)];
+    }
+
+    map.set(nums[index], index);
+  }
+
+  return [-1, -1];
+}
