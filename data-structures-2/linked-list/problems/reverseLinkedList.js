51b70e51e614366d83071d1098793114426aee3d
ShakthivelRameshNirmala <rn.shakthivel@gmail.com>
2025-01-13 08:32:42 -0600

    Reverse Linked List - Recursive

--- data-structures-2/linked-list/problems/reverseLinkedList.js
+++ data-structures-2/linked-list/problems/reverseLinkedList.js
@@ -1,4 +1,16 @@
 function reverseLinkedList(head) {
+  if (head === null || head.next === null) {
+    return head;
+  }
+
+  const newHead = reverseList(head.next);
+  head.next.next = head;
+  head.next = null;
+
+  return newHead;
+}
+
+function reverseLinkedList(head) {
   let prevNode = null,
     currNode = head;
 
