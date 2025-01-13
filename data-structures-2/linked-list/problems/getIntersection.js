ac7bb45c37251931d6c1fca3cd804a32f6716866
ShakthivelRameshNirmala <rn.shakthivel@gmail.com>
2025-01-13 07:58:23 -0600

    Get Intersect - Hash Map

--- data-structures-2/linked-list/problems/getIntersection.js
+++ data-structures-2/linked-list/problems/getIntersection.js
@@ -1,4 +1,24 @@
 function getIntersection(headA, headB) {
+  const seen = new Set();
+
+  while (headB) {
+    seen.add(headB);
+    headB = headB.next;
+  }
+
+  while (headA) {
+    if (seen.has(headA)) {
+      return headA;
+    }
+
+    seen.add(headA);
+    headA = headA.next;
+  }
+
+  return null;
+}
+
+function getIntersection(headA, headB) {
   let pointerA = headA,
     pointerB = headB;
 
