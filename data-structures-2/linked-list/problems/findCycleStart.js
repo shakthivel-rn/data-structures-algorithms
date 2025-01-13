2449074a7959a53654fb1d05a5591fed26fd5f07
ShakthivelRameshNirmala <rn.shakthivel@gmail.com>
2025-01-13 07:44:08 -0600

    Find Cycle Start - Slow Fast Pointer

--- data-structures-2/linked-list/problems/findCycleStart.js
+++ data-structures-2/linked-list/problems/findCycleStart.js
@@ -13,3 +13,37 @@ function findCycleStart(head) {
 
   return null;
 }
+
+function findCycleStart(head) {
+  if (head === null) return head;
+
+  const intersect = getIntersect(head);
+
+  if (intersect === null) return null;
+
+  let pointerA = head,
+    pointerB = intersect;
+
+  while (pointerA !== pointerB) {
+    pointerA = pointerA.next;
+    pointerB = pointerB.next;
+  }
+
+  return pointerA;
+}
+
+function getIntersect(node) {
+  let slow = node,
+    fast = node;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return slow;
+    }
+  }
+
+  return null;
+}
